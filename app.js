93ba714601faf775a8bfca395b397a3cf45348d1
klervi <klervi.le.montreer@gmail.com>
2023-03-30 11:39:27 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const {log} = require('console');
+const path = require("path");
+const app = express();
+const dotenv = require('dotenv').config( {
+    path: path.join(__dirname, '.env')
+  } );
+
+//port par defaut pour nodejs
+const port = process.env.NODE_PORT;
+
+const mysql = require('mysql');
+const connexion = mysql.createConnection({
+    host:'localhost',
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWD,
+    database:process.env.DB_NAME
+})
+
+
+
+app.listen(port, ()=>{
+    log('app lancée sur le port '+ port) ;
+})
+
+app.get('/',(req,res)=>{
+    connexion.query('select * from recette',(err, sql_result)=>{
+        if(err) throw err
+        res.json(sql_result);
+    })
+})
