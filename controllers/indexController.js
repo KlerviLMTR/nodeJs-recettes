8fe70894d16e9452f8be8d3741d880e7be59ff01
klervi <klervi.le.montreer@gmail.com>
2023-04-16 15:09:25 +0200

    fix css

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,9 @@
-// const path = require("path");
-// const app = require("../app.js");
-let ingredientService = require('../services/IngredientService')
-
 const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
   
-  let ingredientTrouves = ingredientService.voirTousLesIngredients(); 
-
-  res.render('index', { ingredients: ingredientTrouves });
+  res.render('index');
 
 });
 
